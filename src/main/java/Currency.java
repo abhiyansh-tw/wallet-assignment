fa2e3056e44fd9e55ad31b0a269d3edd1016df5b
abhiyansh-tw <abhiyansh.arya@thoughtworks.com>
2022-08-09 11:29:33 +0530

    Add enum Currency with functionality to convert to & from base value

---
+++ src/main/java/Currency.java
@@ -0,0 +1,21 @@
+public enum Currency {
+
+    RUPEE(1), DOLLAR(74.85);
+    private final double factor;
+
+    Currency(double factor) {
+        this.factor = factor;
+    }
+
+    public double convertToBaseValue(double value) {
+        return roundOff(value * this.factor);
+    }
+
+    public double convertFromBaseValue(double value) {
+        return roundOff(value / this.factor);
+    }
+
+    private static double roundOff(double value) {
+        return (double) Math.round(value * 100) / 100;
+    }
+}
---
+++ src/test/java/CurrencyTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class CurrencyTest {
+    @Test
+    void shouldConvertToBaseValue() {
+        assertThat(Currency.RUPEE.convertToBaseValue(10.0), is(equalTo(10.0)));
+        assertThat(Currency.DOLLAR.convertToBaseValue(10), is(equalTo(748.5)));
+    }
+
+    @Test
+    void shouldConvertFromBaseValue() {
+        assertThat(Currency.RUPEE.convertFromBaseValue(10.0), is(equalTo(10)));
+        assertThat(Currency.DOLLAR.convertFromBaseValue(50.0), is(equalTo(0.67)));
+    }
+}
