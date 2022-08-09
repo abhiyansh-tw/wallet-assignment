0f407651efd0c383066491e3932bb7db7cb34ef2
abhiyansh-tw <abhiyansh.arya@thoughtworks.com>
2022-08-09 11:33:27 +0530

    add currency in money & factory methods to create rupee and dollar

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,27 +1,37 @@
 import java.util.Objects;
 
 public class Money {
-    private final int value;
+    private final double value;
+    private final Currency currency;
 
-    public Money(int value) {
+    private Money(double value, Currency currency) {
         this.value = value;
+        this.currency = currency;
+    }
+
+    public static Money createRupee(double value){
+        return new Money(value, Currency.RUPEE);
+    }
+
+    public static Money createDollar(double value){
+        return new Money(value, Currency.DOLLAR);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Money money = (Money) o;
-        return value == money.value;
+        Money that = (Money) o;
+        return this.currency.convertToBaseValue(this.value) == that.currency.convertToBaseValue(that.value);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value);
+        return Objects.hash(value, currency);
     }
 
     public Money add(Money that) {
-        int totalValue = this.value + that.value;
-        return new Money(totalValue);
+        double totalBaseValue = this.currency.convertToBaseValue(this.value) + that.currency.convertToBaseValue(that.value);
+        return new Money(this.currency.convertFromBaseValue(totalBaseValue), this.currency);
     }
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -5,22 +5,46 @@ import static org.hamcrest.Matchers.*;
 
 public class MoneyTest {
     @Test
-    void shouldEquateTwoMoneyWithSameValue() {
-        assertThat(new Money(100), is(equalTo((new Money(100)))));
+    void shouldEquateTwoMoneyWithSameCurrencyAndSameValue() {
+        assertThat(Money.createRupee(100), is(equalTo((Money.createRupee(100)))));
+        assertThat(Money.createDollar(100), is(equalTo((Money.createDollar(100)))));
     }
 
     @Test
-    void shouldNotEquateTwoMoneyWithDifferentValue() {
-        assertThat(new Money(100), is(not(equalTo(new Money(200)))));
+    void shouldNotEquateTwoMoneyWithSameCurrencyAndDifferentValue() {
+        assertThat(Money.createRupee(100), is(not(equalTo(Money.createRupee(200)))));
+        assertThat(Money.createDollar(100), is(not(equalTo(Money.createDollar(200)))));
     }
 
     @Test
-    void shouldReturnMoneyOfValueTwoWhenTwoMoneyOfValueOneAreAdded() {
-        Money money = new Money(1);
-        Money anotherMoney = new Money(1);
-        Money expectedSum = new Money(2);
+    void shouldNotEquateTwoMoneyWithInEquivalentValue() {
+        assertThat(Money.createRupee(100), is(not(equalTo((Money.createDollar(100))))));
+        assertThat(Money.createRupee(374.24), is(not(equalTo((Money.createDollar(5))))));
+    }
 
-        Money actualSum = money.add(anotherMoney);
+    @Test
+    void shouldEquateTwoMoneyWithEquivalentValue() {
+        assertThat(Money.createRupee(374.25), is(equalTo((Money.createDollar(5)))));
+    }
+
+    @Test
+    void shouldReturnThreeRupeeWhenTwoRupeeAndOneRupeeAreAdded() {
+        Money oneRupee = Money.createRupee(1);
+        Money twoRupee = Money.createRupee(2);
+        Money threeRupee = Money.createRupee(3);
+
+        Money actualSum = oneRupee.add(twoRupee);
+
+        assertThat(actualSum, is(equalTo(threeRupee)));
+    }
+
+    @Test
+    void shouldReturnSeventyFivePointEightFiveRupeeWhenOneRupeeAndOneDollarAreAdded() {
+        Money oneRupee = Money.createRupee(1);
+        Money oneDollar = Money.createDollar(1);
+        Money expectedSum = Money.createRupee(75.85);
+
+        Money actualSum = oneRupee.add(oneDollar);
 
         assertThat(actualSum, is(equalTo(expectedSum)));
     }
